dde323cb752c7a26c74c34459f1c4482a62b4fdc
David Hedley <davidjhedley1@gmail.com>
2025-10-12 19:04:39 +0100

    UPDATE: Added improved version of Leetcode Two Sum

--- javascript/leetcode/easy/1-two-sum.js
+++ javascript/leetcode/easy/1-two-sum.js
@@ -27,6 +27,39 @@ var twoSum = function(nums, target) {
     return positions;
 };
 
+var twoSumBetter = function( nums, target ) {
+
+    let numbers   = new Set( nums );
+    let positions = [];
+
+    for( let i = 0; i < nums.length; i++ ) {
+
+        let num = nums[i];
+
+        let diff = target - num;
+
+        if( numbers.has( diff ) ) {
+
+            let index = nums.indexOf( diff, i + 1 );
+
+            if( index > -1 ) {
+
+                positions.push( i, index );
+
+                return positions;
+            }
+        }
+    }
+
+    return positions;
+}
+
 console.log( twoSum( [2,7,11,15], 9 ) );
+console.log( twoSumBetter( [2,7,11,15], 9 ) );
 
 // [0,1]
+
+console.log( twoSum( [3,2,4], 6 ) );
+console.log( twoSumBetter( [3,2,4], 6 ) );
+
+// [1,2]
